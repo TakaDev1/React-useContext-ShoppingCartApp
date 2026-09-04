5732c873748d56ca3d8a1123fe8081ef1a4ccea3
Izumi Takayuki <r8quattrospider@gmail.com>
2026-09-04 13:57:17 +0900

    feat: Created CartContext Component

---
+++ src/contexts/CartContext.tsx
@@ -0,0 +1,33 @@
+import React, { createContext, useState } from "react";
+
+interface ProductType {
+  id: number;
+  item: string;
+}
+
+interface CartContextType {
+  cart: ProductType[];
+  addItem: (product: ProductType) => void;
+  removeItem: (id: number) => void;
+}
+
+const CartContext = createContext<CartContextType | undefined>(undefined);
+
+const CartProvider = ({ children }: { children: React.ReactNode }) => {
+  const [cart, setCart] = useState<ProductType[]>([]);
+
+  const addItem = (product: ProductType) => {
+    return setCart((prev) => [...prev, product]);
+  };
+
+  const removeItem = (id: number) => {
+    setCart((prev) => prev.filter((product) => product.id !== id));
+  };
+  return (
+    <>
+      <CartContext.Provider value={{ cart, addItem, removeItem }}>{children}</CartContext.Provider>
+    </>
+  );
+};
+
+export default CartContext;
